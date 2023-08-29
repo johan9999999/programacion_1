f4969594f2f1b06807ca687d04d359548c82abc4
johan9999999 <johanpradapineda14@gmail.com>
2023-08-29 15:23:09 -0500

    carpeta nueva

--- trabajo1.py
+++
@@ -1,9 +0,0 @@
-def letraV(letra):
-    return letra.lower() in ['a', 'e', 'i', 'o', 'u']
-
-let = input("Pon una letra del abecedario: ")
-
-if letraV(let):
-    print(let," es vocal.")
-else:
-    print(let," es consonante.")
